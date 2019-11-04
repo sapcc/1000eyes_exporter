91473e5c89b768abcc1dbfb11fadfd7d0d29e2d1
Tilo Geissler <tilo.geissler@sap.com>
2019-11-04 09:13:34 +0100

    [thoudandeyes-exporter] parallel requests

---
+++ thousandeyes.go
@@ -0,0 +1,240 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+	"reflect"
+)
+
+const (
+	apiURLalerts          = "https://api.thousandeyes.com/v6/alerts?format=json"
+	apiURLTests           = "https://api.thousandeyes.com/v6/tests.json"
+	apiURLTestBGB         = "https://api.thousandeyes.com/v6/net/bgp-metrics/%d.json"
+	apiURLTestHTTP        = "https://api.thousandeyes.com/v6/web/http-server/%d.json"
+	apiURLTestHTTPMetrics = "https://api.thousandeyes.com/v6/net/metrics/%d.json"
+)
+
+const (
+	alerts = iota
+)
+
+// ThousandeyesRequest bla
+type ThousandeyesRequest struct {
+	URL            string
+	ResponseCode   int
+	ResponseObject interface{}
+	Error          error
+}
+
+// ThousandAlerts describes the JSON returned by a request active alerts to ThousandEyes
+type ThousandAlerts struct {
+	From  string `json:"from"`
+	Alert []struct {
+		Active    int    `json:"active"`
+		AlertID   int    `json:"alertId"`
+		DateEnd   string `json:"dateEnd,omitempty"`
+		DateStart string `json:"dateStart"`
+		Monitors  []struct {
+			Active         int    `json:"active"`
+			MetricsAtStart string `json:"metricsAtStart"`
+			MetricsAtEnd   string `json:"metricsAtEnd"`
+			MonitorID      int    `json:"monitorId"`
+			MonitorName    string `json:"monitorName"`
+			PrefixID       int    `json:"prefixId"`
+			Prefix         string `json:"prefix"`
+			DateStart      string `json:"dateStart"`
+			DateEnd        string `json:"dateEnd"`
+			Permalink      string `json:"permalink"`
+			Network        string `json:"network"`
+		} `json:"monitors,omitempty"`
+		Permalink      string `json:"permalink"`
+		RuleExpression string `json:"ruleExpression"`
+		RuleID         int    `json:"ruleId"`
+		RuleName       string `json:"ruleName"`
+		TestID         int    `json:"testId"`
+		TestName       string `json:"testName"`
+		ViolationCount int    `json:"violationCount"`
+		Type           string `json:"type"`
+		APILinks       []struct {
+			Rel  string `json:"rel"`
+			Href string `json:"href"`
+		} `json:"apiLinks,omitempty"`
+		Agents []struct {
+			Active         int    `json:"active"`
+			MetricsAtStart string `json:"metricsAtStart"`
+			MetricsAtEnd   string `json:"metricsAtEnd"`
+			AgentID        int    `json:"agentId"`
+			AgentName      string `json:"agentName"`
+			DateStart      string `json:"dateStart"`
+			DateEnd        string `json:"dateEnd"`
+			Permalink      string `json:"permalink"`
+		} `json:"agents,omitempty"`
+	} `json:"alert"`
+	Pages struct {
+		Current int `json:"current"`
+	} `json:"pages"`
+}
+
+//ThousandTests describes needed Fields from the JSON returned by a request  to ThousandEyes
+type ThousandTests struct {
+	Tests []ThousandTest `json:"test"`
+}
+
+//ThousandTest in detail
+type ThousandTest struct {
+	TestID   int    `json:"testId"`
+	TestName string `json:"testName"`
+	Type     string `json:"type"`
+	Prefix   string `json:"prefix"`
+	Interval int    `json:"interval"`
+	URL      string `json:"url"`
+}
+
+//https://api.thousandeyes.com/v6/net/bgp-metrics/557962.json
+
+// BGPTestResults BGP Test details
+type BGPTestResults struct {
+	Net struct {
+		Test       ThousandTest `json:"test"`
+		BgpMetrics []struct {
+			CountryID    string  `json:"countryId"`
+			Prefix       string  `json:"prefix"`
+			MonitorName  string  `json:"monitorName"`
+			Reachability float32 `json:"reachability"`
+			Updates      float32 `json:"updates"`
+			PathChanges  float32 `json:"pathChanges"`
+		} `json:"bgpMetrics"`
+	} `json:"net"`
+}
+
+// https://api.thousandeyes.com/v6/net/metrics/612434.json
+
+// HTTPTestMetricResults HTTP Test details on network metrics
+type HTTPTestMetricResults struct {
+	Net struct {
+		Test        ThousandTest `json:"test"`
+		HTTPMetrics []struct {
+			AvgLatency float32 `json:"avgLatency"`
+			Loss       float32 `json:"loss"`
+			MaxLatency float32 `json:"maxLatency"`
+			Jitter     float32 `json:"jitter"`
+			MinLatency float32 `json:"minLatency"`
+			ServerIP   string  `json:"serverIp"`
+			AgentName  string  `json:"agentName"`
+			CountryID  string  `json:"countryId"`
+		} `json:"metrics"`
+	} `json:"net"`
+}
+
+// HTTPTestWebServerResults HTTP Test details on Server Response
+type HTTPTestWebServerResults struct {
+	Web struct {
+		Test       ThousandTest `json:"test"`
+		HTTPServer []struct {
+			ConnectTime  int    `json:"connectTime"`
+			DNSTime      int    `json:"dnsTime"`
+			ErrorType    string `json:"errorType"`
+			NumRedirects int    `json:"numRedirects"`
+			ReceiveTime  int    `json:"receiveTime"`
+			ResponseCode int    `json:"responseCode"`
+			ResponseTime int    `json:"responseTime"`
+			TotalTime    int    `json:"totalTime"`
+			WaitTime     int    `json:"waitTime"`
+			WireSize     int    `json:"wireSize"`
+			AgentName    string `json:"agentName"`
+			CountryID    string `json:"countryId"`
+			Date         string `json:"date"`
+			AgentID      int    `json:"agentId"`
+			RoundID      int    `json:"roundId"`
+		} `json:"httpServer"`
+	} `json:"web"`
+}
+
+func thousandEyesDateTime() string {
+	// Go back a bit to have some alerts to parse
+	t := time.Now().UTC().Add(-*retrospectionPeriod)
+	// 2006-01-02T15:04:05 is a magic date to format dates using example based layouts
+	f := t.Format("2006-01-02T15:04:05")
+	return string(f)
+}
+
+func (t *thousandEyes) GetAlerts() (ThousandAlerts, error) {
+
+	r := ThousandeyesRequest{
+		URL:            apiURLalerts,
+		ResponseObject: new(ThousandAlerts),
+	}
+
+	CallSingle(t.token, &r)
+
+	if r.Error != nil {
+		return r.ResponseObject.(ThousandAlerts), r.Error
+	}
+
+	return r.ResponseObject.(ThousandAlerts), nil
+}
+
+func (t *thousandEyes) GetTests() ([]BGPTestResults, []HTTPTestMetricResults, []HTTPTestWebServerResults, error) {
+
+	var httpMs []HTTPTestMetricResults
+	var httpWs []HTTPTestWebServerResults
+	var bgpMs []BGPTestResults
+
+	rTests := ThousandeyesRequest{
+		URL:            apiURLTests,
+		ResponseObject: new(ThousandTests),
+	}
+	CallSingle(t.token, &rTests)
+	if rTests.Error != nil {
+		return bgpMs, httpMs, httpWs, rTests.Error
+	}
+
+	te := rTests.ResponseObject.(*ThousandTests)
+
+	var testRequests []ThousandeyesRequest
+
+	log.Println(fmt.Sprintf("Test Count: %d", len(te.Tests)))
+
+	for i := range te.Tests {
+		switch te.Tests[i].Type {
+		case "http-server":
+			testRequests = append(testRequests, ThousandeyesRequest{
+				URL:            fmt.Sprintf(apiURLTestHTTP, te.Tests[i].TestID),
+				ResponseObject: new(HTTPTestWebServerResults),
+			})
+			testRequests = append(testRequests, ThousandeyesRequest{
+				URL:            fmt.Sprintf(apiURLTestHTTPMetrics, te.Tests[i].TestID),
+				ResponseObject: new(HTTPTestMetricResults),
+			})
+		case "bgp":
+			testRequests = append(testRequests, ThousandeyesRequest{
+				URL:            fmt.Sprintf(apiURLTestBGB, te.Tests[i].TestID),
+				ResponseObject: new(BGPTestResults),
+			})
+		default:
+			log.Fatal(fmt.Sprintf("%s.\n", te.Tests[i].Type))
+		}
+	}
+
+	//CallSequence(t.token, testRequests)
+	CallParallel(t.token, testRequests)
+
+	for c, o := range testRequests {
+
+		//switch v:=o.ResponseObject.(type) {
+		//v := reflect.TypeOf(o.ResponseObject)
+		switch o.ResponseObject.(type) {
+			case *BGPTestResults:
+				bgpMs = append(bgpMs,*testRequests[c].ResponseObject.(*BGPTestResults))
+			case *HTTPTestMetricResults:
+				httpMs = append(httpMs,*testRequests[c].ResponseObject.(*HTTPTestMetricResults))
+			case *HTTPTestWebServerResults:
+				httpWs = append(httpWs,*testRequests[c].ResponseObject.(*HTTPTestWebServerResults))
+			default:
+				log.Fatal(fmt.Sprintf("Not a handled test type %s (%d of %d). Bug. Fix Code.", reflect.TypeOf(o.ResponseObject), c, len(testRequests)))
+		}
+	}
+
+	return bgpMs, httpMs, httpWs, nil
+}
--- thousandeyes_api.go
+++ thousandeyes_api.go
@@ -4,96 +4,99 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"time"
+	"sync"
 )
+var bHitRateLimit bool
 
-// ThousandAlerts describes the JSON returned by a request active alerts to ThousandEyes
-type ThousandAlerts struct {
-	From  string `json:"from"`
-	Alert []struct {
-		Active    int    `json:"active"`
-		AlertID   int    `json:"alertId"`
-		DateEnd   string `json:"dateEnd,omitempty"`
-		DateStart string `json:"dateStart"`
-		Monitors  []struct {
-			Active         int    `json:"active"`
-			MetricsAtStart string `json:"metricsAtStart"`
-			MetricsAtEnd   string `json:"metricsAtEnd"`
-			MonitorID      int    `json:"monitorId"`
-			MonitorName    string `json:"monitorName"`
-			PrefixID       int    `json:"prefixId"`
-			Prefix         string `json:"prefix"`
-			DateStart      string `json:"dateStart"`
-			DateEnd        string `json:"dateEnd"`
-			Permalink      string `json:"permalink"`
-			Network        string `json:"network"`
-		} `json:"monitors,omitempty"`
-		Permalink      string `json:"permalink"`
-		RuleExpression string `json:"ruleExpression"`
-		RuleID         int    `json:"ruleId"`
-		RuleName       string `json:"ruleName"`
-		TestID         int    `json:"testId"`
-		TestName       string `json:"testName"`
-		ViolationCount int    `json:"violationCount"`
-		Type           string `json:"type"`
-		APILinks       []struct {
-			Rel  string `json:"rel"`
-			Href string `json:"href"`
-		} `json:"apiLinks,omitempty"`
-		Agents []struct {
-			Active         int    `json:"active"`
-			MetricsAtStart string `json:"metricsAtStart"`
-			MetricsAtEnd   string `json:"metricsAtEnd"`
-			AgentID        int    `json:"agentId"`
-			AgentName      string `json:"agentName"`
-			DateStart      string `json:"dateStart"`
-			DateEnd        string `json:"dateEnd"`
-			Permalink      string `json:"permalink"`
-		} `json:"agents,omitempty"`
-	} `json:"alert"`
-	Pages struct {
-		Current int `json:"current"`
-	} `json:"pages"`
-}
-
-func thousandEyesDateTime() string {
-	// Go back a bit to have some alerts to parse
-	t := time.Now().UTC().Add(-*retrospectionPeriod)
-	// 2006-01-02T15:04:05 is a magic date to format dates using example based layouts
-	f := t.Format("2006-01-02T15:04:05")
-	return string(f)
-}
-
-func (t *thousandEyes) getAlerts() (ThousandAlerts, error) {
-
-	var a ThousandAlerts
+// CallSingle is a single URL call
+// it returns true, if the API Rate Limit was hit
+// the error & result object itself are modified in the Request struct
+func CallSingle(token string, request *ThousandeyesRequest) bool {
 
 	client := &http.Client{}
+	bHitAPILimit := false
 
-	url := string("https://api.thousandeyes.com/v6/alerts?format=json")
-	if retrospectionPeriod.Seconds() > 0 {
-		url += "&from=" + thousandEyesDateTime()
-	}
-
-	req, err := http.NewRequest("GET", url, nil)
-	req.Header.Add("Authorization", "Bearer "+t.token)
+	req, err := http.NewRequest("GET", request.URL, nil)
+	req.Header.Add("Authorization", "Bearer "+token)
 	req.Header.Add("Content-Type", "application/json")
+
+	log.Println(fmt.Sprintf("CALL >>> Url: %s", request.URL))
 	resp, err := client.Do(req)
-	if err != nil || resp.StatusCode != 200 {
-		return a, fmt.Errorf("ThousandEyes API Request failed: %s / http code: %d", err, resp.StatusCode)
+	if resp.StatusCode == 429 {
+		bHitAPILimit = true
+		request.Error = fmt.Errorf("ThousandEyes API Rate Limit hit (\"Too many requests\") - http code: %d", resp.StatusCode)
+		return bHitAPILimit
+	} else if err != nil || resp.StatusCode != 200 {
+		request.Error = fmt.Errorf("ThousandEyes API Request failed: %s / http code: %d", err, resp.StatusCode)
+		return bHitAPILimit
 	}
 	defer resp.Body.Close()
-
 	responseData, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return a, err
+		request.Error = err
+		return bHitAPILimit
 	}
-	err = json.Unmarshal(responseData, &a)
+	//log.Println(fmt.Sprintf("\nCALL <<< Url: %s |\n%s", request.URL, string(responseData)))
+	err = json.Unmarshal(responseData, request.ResponseObject)
 	if err != nil {
-		fmt.Print(err.Error())
-		return a, fmt.Errorf("parse configuration: %s", err.Error())
+		log.Println(err.Error())
+		request.Error = fmt.Errorf("parse configuration: %s", err.Error())
 	}
-	return a, nil
 
+	return bHitAPILimit
+}
+
+// CallSequence does CallSingle calls one after the other
+// it returns true, if the API Rate Limit was hit
+// the error & result object itself are modified in the Request struct
+func CallSequence(token string, requests []ThousandeyesRequest) bool {
+
+	for c := range requests {
+		if CallSingle(token, &requests[c]) {
+			return true
+		}
+	}
+	return false
+}
+
+// CallParallel does CallSingle calls in parallel - can hit thousandeyes api restrictions easily
+// it returns true, if the API Rate Limit was hit
+// the error & result object itself are modified in the Request struct
+func CallParallel(token string, requests []ThousandeyesRequest) error {
+
+	var waitGroup sync.WaitGroup
+	var m sync.Mutex
+
+	httpChan := make(chan ThousandeyesRequest, len(requests))
+	bHitRateLimit = false;
+
+	for c, request := range requests {
+		log.Println(fmt.Sprintf("Count [%d] - URL: %s", c, request.URL))
+
+		waitGroup.Add(1)
+
+		go func(token string, request ThousandeyesRequest, httpChan chan (ThousandeyesRequest), m *sync.Mutex) {
+			defer waitGroup.Done()
+
+			if (bHitRateLimit){
+				log.Fatal(fmt.Sprintf("Skip Detail request (%s), bcz we hit the API Request Limit.", request.URL))
+				return
+			}
+			//log.Println(fmt.Sprintf("URL: %s | API-Request-Limit-Hit ?: %t", request.URL, b))
+
+			b := CallSingle(token, &request)
+			m.Lock()
+			bHitRateLimit = b
+			m.Unlock()
+			httpChan <- request
+
+		}(token, request, httpChan, &m)
+	}
+
+	waitGroup.Wait()
+	close(httpChan)
+
+	return nil
 }
